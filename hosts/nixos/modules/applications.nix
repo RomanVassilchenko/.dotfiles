da90466c6790029a28d3b65c04b40439355ce8e5
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2024-08-24 00:51:28 +0500

    personal changes (new packages)

--- hosts/nixos/modules/applications.nix
+++ hosts/nixos/modules/applications.nix
@@ -4,6 +4,8 @@
 
   programs.firefox.enable = true;
 
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
   environment.systemPackages = with pkgs; [
     git
     google-chrome
@@ -17,26 +19,26 @@
     libreoffice-qt6-fresh
     thunderbird
     krita
-    goldwarden
+    bitwarden-desktop
     kdePackages.kdenlive
     gnome.gnome-boxes
     inkscape-with-extensions
     postman
     dbeaver-bin
     papirus-icon-theme
-#     jetbrains.goland
+    jetbrains.goland
     fastfetch
     neovim
     insync
     zed-editor
     boxbuddy
+    fzf
+    zoxide
 
     kdePackages.breeze
     kdePackages.breeze-gtk
     kdePackages.breeze-grub
     kdePackages.breeze-icons
     kdePackages.breeze-plymouth
-
-    # home-manager
   ];
 }
