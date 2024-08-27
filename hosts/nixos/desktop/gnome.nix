9fd43e2c001ad00299ebdeb971439bb65924158a
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2024-08-27 23:14:14 +0500

    Personal changes

--- hosts/nixos/desktop/gnome.nix
+++ hosts/nixos/desktop/gnome.nix
@@ -28,7 +28,6 @@
     drawing
     amberol
     parabolic
-    gnome-network-displays
 
     gnomeExtensions.blur-my-shell
     gnomeExtensions.dash-to-dock
--- hosts/nixos/modules/networking.nix
+++ hosts/nixos/modules/networking.nix
@@ -5,12 +5,15 @@
   networking.hostName = "XiaoXinPro";
 
   # networking.firewall = {
-#     enable = true;
-#     allowedTCPPortRanges = [
-#       { from = 1714; to = 1764; } # KDE Connect
-#     ];
-#     allowedUDPPortRanges = [
-#       { from = 1714; to = 1764; } # KDE Connect
-#     ];
-#   };
+  #   enable = true;
+  #   allowedTCPPortRanges = [
+  #     { from = 1714; to = 1764; } # KDE Connect
+  #   ];
+  #   allowedUDPPortRanges = [
+  #     { from = 1714; to = 1764; } # KDE Connect
+  #   ];
+
+  #   allowedTCPPorts = [7236 7250];
+  #   allowedUDPPorts = [7236 5353];
+  # };
 }
