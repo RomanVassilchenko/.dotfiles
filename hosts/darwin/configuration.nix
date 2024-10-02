44cb8434f5207ae82af80c98240b7bc40abfb573
rovasilchenko <rovasilchenko@ozon.ru>
2024-10-02 10:28:21 +0500

    Working changes: Add arc browser

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -85,23 +85,23 @@
         # Appearance
         AppleInterfaceStyle = "Dark"; # dark mode
 
-        # AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
-        # ApplePressAndHoldEnabled = true; # enable press and hold
+        AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
+        ApplePressAndHoldEnabled = true; # enable press and hold
 
         # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
         # This is very useful for vim users, they use `hjkl` to move cursor.
         # sets how long it takes before it starts repeating.
-        # InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
         # sets how fast it repeats once it starts.
-        # KeyRepeat = 3; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+        KeyRepeat = 3; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
 
-        # NSAutomaticCapitalizationEnabled = false; # disable auto capitalization(自动大写)
-        # NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution(智能破折号替换)
-        # NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution(智能句号替换)
-        # NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution(智能引号替换)
-        # NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
-        # NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
-        # NSNavPanelExpandedStateForSaveMode2 = true;
+        NSAutomaticCapitalizationEnabled = false; # disable auto capitalization(自动大写)
+        NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution(智能破折号替换)
+        NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution(智能句号替换)
+        NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution(智能引号替换)
+        NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
+        NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
+        NSNavPanelExpandedStateForSaveMode2 = true;
       };
 
       # customize settings that not supported by nix-darwin directly
@@ -229,6 +229,7 @@
     "obs"
     "rectangle"
     "zen-browser"
+    "arc"
   ];
   homebrew.brews = [
     "go"
