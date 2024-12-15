155dbbae5614aa48dcf1d37fc5e68cab10a6ee2d
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2024-12-15 16:06:52 +0500

    Personal changes: Add OpenSSH configuration to HomeServer and remove redundant settings from services module for improved security and management

--- hosts/HomeServer/default.nix
+++ hosts/HomeServer/default.nix
@@ -4,4 +4,10 @@
     ./../../modules/core
     ./../../modules/nixos/default.homeserver.nix
   ];
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+  services.openssh.passwordAuthentication = true;
+  services.openssh.port = 22;
+  services.openssh.protocol = "2";
 }
--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -4,16 +4,5 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
-    openssh = {
-      enable = true;
-      ports = [ 22 ];
-      settings = {
-      PasswordAuthentication = true;
-      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-    };
-};
   };
 }
