4e8fb7461b0d5df5a3f9da70a5e8691ba95d6c73
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2024-12-15 16:11:34 +0500

    Personal changes: Update OpenSSH configuration to use ports array for improved flexibility

--- hosts/HomeServer/default.nix
+++ hosts/HomeServer/default.nix
@@ -8,6 +8,6 @@
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "no";
   services.openssh.passwordAuthentication = true;
-  services.openssh.port = 22;
+  services.openssh.ports = [ 22 ];
   services.openssh.protocol = "2";
 }
